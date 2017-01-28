2e8c8bd53217df6f24f8febce46ddd50c8398d7b
Stefan Langenbach <stefan.buenten@gmail.com>
2017-01-28 14:20:58 +0100

    Switched to GNU R and edited configuration parameters and required packages

--- p4/r_packages.r
+++ p4/r_packages.r
@@ -5,4 +5,5 @@ install.packages("devtools", dependencies = T)
 # ggplot2, dplyr, tidyr, readr, purrr, tibble
 install.packages("tidyverse", dependencies = T)
 
-# Install additional R packages
+# Install additional R packages for nanodegree
+install.packages("alr3", dependencies = T)
