706187fdcf036240df7fe64cd01c2981659e477e
Stefan Langenbach <stefan.buenten@gmail.com>
2017-02-14 09:48:35 +0100

    Continued with problem set 5

--- p4/lesson5/problem_set_lesson5.r
+++ p4/lesson5/problem_set_lesson5.r
@@ -2,4 +2,23 @@
 library(tidyverse)
 
 # load data
+data("diamonds")
+  
+# histogram of diamond prices
+ggplot(data = diamonds, aes(x = price, color = cut)) + 
+  geom_histogram() + 
+  facet_wrap( ~ color)
 
+# scatterplot of diamond prices vs table
+ggplot(data = diamonds, aes(x = table, y = price, color = cut)) +
+  geom_point()
+
+# scatterplot of diamond prices vs volume
+diamonds_top99 <- subset(diamonds, top_99 <- quantile(top_99))
+
+ggplot(data = diamonds_top99)
+       aes(x = x*y*z,
+           y = price,
+           color = clarity)) +
+  geom_point() +
+  scale_x_log10()
