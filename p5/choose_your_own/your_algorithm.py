b66c442904b6e735270226bdf450949c201d4b3e
Stefan Langenbach <stefan.buenten@gmail.com>
2017-04-06 09:16:55 +0200

    Continued with lesson 5

--- p5/choose_your_own/your_algorithm.py
+++ p5/choose_your_own/your_algorithm.py
@@ -18,36 +18,36 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 # knn
-algo = "KNN"
-from sklearn.neighbors import KNeighborsClassifier
-clf = KNeighborsClassifier()
+# algo = "KNN"
+# from sklearn.neighbors import KNeighborsClassifier
+# clf = KNeighborsClassifier()
 
-# # adaboost
+# adaboost
 # algo = "Adaboost"
 # from sklearn.ensemble import AdaBoostClassifier
 # clf = AdaBoostClassifier()
-#
-# # random forrest
-# algo = "Random Forest"
-# from sklearn.ensemble import RandomForestClassifier
-# clf = RandomForestClassifier()
+
+# random forrest
+algo = "Random Forest"
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier()
 
 # fit
 clf.fit(features_train, labels_train)
 
 # predict
-pred = clf.predict(labels_test)
+pred = clf.predict(features_test)
 
 # print accuracy
 from sklearn.metrics import accuracy_score
