6bae2125a7ae8116d2ad319bf23c715d379c5311
Stefan Langenbach <stefan.buenten@gmail.com>
2017-04-11 13:26:53 +0200

    Completed outliers section

--- p5/outliers/enron_outliers.py
+++ p5/outliers/enron_outliers.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+from __future__ import print_function
 
+import numpy as np
 import pickle
 import sys
 import matplotlib.pyplot
@@ -10,10 +12,31 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+
+# remove TOTAL line from dict
+data_dict.pop("TOTAL", 0)
+
+# prepare data
 data = featureFormat(data_dict, features)
 
 
 ### your code below
 
+# for point in data:
+#     salary = point[0]
+#     bonus = point[1]
+#     matplotlib.pyplot.scatter( salary, bonus )
+#
+# matplotlib.pyplot.xlabel("salary")
+# matplotlib.pyplot.ylabel("bonus")
+# matplotlib.pyplot.show()
 
+# print information for TOTAL key
+#print(data_dict["TOTAL"])
+
+# print information for remaining outliers
+for k,v in data_dict.iteritems():
+    if v["salary"] != "NaN" and v["bonus"] != "NaN":
+        if v["salary"] >= 1000000 and v["bonus"] >= 5000000:
+            print(k, v["salary"], v["bonus"])
 
