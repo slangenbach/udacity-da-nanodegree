5d5531b539e7186eb0c0c723ecec5bcc836f5b89
Stefan Langenbach <stefan.buenten@generali.com>
2017-06-01 18:57:57 +0200

    Continued working on final project for p5

--- p5/final_project/poi_id.py
+++ p5/final_project/poi_id.py
@@ -14,9 +14,7 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary', 'bonus', 'total_payments', 'exercised_stock_options', "total_stock_value", "expenses",
-                 "restricted_stock", 'from_this_person_to_poi', 'shared_receipt_with_poi', 'from_poi_to_this_person',
-                 "poi_comm_index", "performance_compensation"]
+features_list = ["poi", "exercised_stock_options", "total_stock_value", "bonus"]
 
 ### Task 2: Remove outliers # ToDo: get rid of outliers
 # remove TOTAL line
@@ -29,8 +27,6 @@ for k, v in data_dict.iteritems():
             v[k2] = 0
 
 # plot features # ToDo evaluate feature importance
-# done in ipython poi_id jupyter notebook
-
 
 ### Task 3: Create new feature(s)
 for k, v in data_dict.iteritems():
@@ -56,34 +52,38 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectFromModel
 from sklearn.decomposition import PCA
-#from sklearn.model_selection import StratifiedShuffleSplit # Todo Implement
+from sklearn.model_selection import StratifiedShuffleSplit # Todo Implement
 from sklearn.model_selection import train_test_split
 
 # scale features
-features = StandardScaler().fit_transform(features)
-
-# apply stratified shuffle split
-#cv = StratifiedShuffleSplit
+#features = StandardScaler().fit_transform(features)
+features = MinMaxScaler().fit_transform(features)
 
 # perform train-test split
 features_train, features_test, labels_train, labels_test = train_test_split(features, labels,test_size=0.3, random_state=42)
 
 # base clf
+from sklearn.linear_model import LinearRegression
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LinearRegression
 
 pipe1 = Pipeline([
     ("nb", GaussianNB()) # provided as base clf
 ])
+
+
 
 # clf with feature selection
 # http://scikit-learn.org/stable/modules/feature_selection.html#univariate-feature-selection
 from sklearn.svm import LinearSVC
 
 
-pipe2 = Pipeline([
+pipe2a = Pipeline([
     ("feautre_selection", SelectFromModel(LinearSVC())),
     ("clf", GaussianNB())
 ])
@@ -91,9 +91,16 @@ pipe2 = Pipeline([
 # clf with feature selection and tree
 from sklearn.ensemble import RandomForestClassifier
 
-pipe3 = Pipeline([
-    ("feautre_selection", SelectKBest()),
+pipe2a = Pipeline([
+    ("feautre_selection", SelectKBest(k=4)),
     ("clf", RandomForestClassifier())
+])
+
+#
+from sklearn.linear_model import LogisticRegressionCV
+cv = StratifiedShuffleSplit(n_splits=10)
+pipe2c = Pipeline([
+    ("log_reg", LogisticRegressionCV(cv=cv, solver="liblinear")) # provided as base clf
 ])
 
 
@@ -106,26 +113,29 @@ pipe3 = Pipeline([
 from sklearn.model_selection import GridSearchCV
 #clf = RandomForestClassifier()
 #params = ""
-#cv = StratifiedShuffleSplit(n_splits=3)
+
 #gs = GridSearchCV(clf, params=params, )
 
 
 # Example starting point. Try investigating other evaluation techniques!
 # fit clf
-clf = pipe2
-clf.fit(features_train, labels_train)
+clf = pipe1
+#clf.fit(features_train, labels_train)
+clf.fit(features, labels)
 
 # predict results
-pred = clf.predict(features_test)
+#pred = clf.predict(features_test)
+pred = clf.predict(features)
 
 # print precision and recall score
-from sklearn.metrics import precision_score, recall_score
-print "precision: %f" % precision_score(pred, labels_test) # base score: 0.4
-print "recall: %f" % recall_score(pred, labels_test) # base score: 0.4
+from sklearn.metrics import precision_score, recall_score, classification_report
+#print "precision: %f" % precision_score(pred, labels_test) # base score: 0.4
+#print "recall: %f" % recall_score(pred, labels_test) # base score: 0.4
+#print classification_report(labels_test, pred)
+print classification_report(labels, pred)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-
 #dump_classifier_and_data(clf, my_dataset, features_list)
