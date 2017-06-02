8ab5e9b7f78161c5fce1efd381c70d2b14e66cf8
Stefan Langenbach <stefan.buenten@gmail.com>
2017-06-02 18:10:50 +0200

    Continued working on final project for p5

--- p5/final_project/poi_id.py
+++ p5/final_project/poi_id.py
@@ -14,9 +14,10 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ["poi", "exercised_stock_options", "total_stock_value", "bonus"]
+# features_list = ["poi", "bonus", "exercised_stock_options", "from_poi_to_this_person", "long_term_incentive", "shared_receipt_with_poi", "total_stock_value"]
+feature_list = ["poi", ] # ToDo: Include all features and let clf decide with to use
 
-### Task 2: Remove outliers # ToDo: get rid of outliers
+### Task 2: Remove outliers
 # remove TOTAL line
 data_dict.pop("TOTAL", 0)
 
@@ -24,19 +25,17 @@ data_dict.pop("TOTAL", 0)
 for k, v in data_dict.iteritems():
     for k2, v2 in v.iteritems():
         if v2 == "NaN":
-            v[k2] = 0
+            #v[k2] = 0
+            pass
 
-# plot features # ToDo evaluate feature importance
+# remove outliers identified during analysis
+data_dict.pop("LAY KENNETH L", 0)
+data_dict.pop("SKILLING JEFFREY K", 0)
+data_dict.pop("LAVORATO JOHN J", 0)
+data_dict.pop("BELFER ROBERT", 0)
 
 ### Task 3: Create new feature(s)
-for k, v in data_dict.iteritems():
-        v["performance_compensation"] = v["bonus"] + v["exercised_stock_options"] + v["long_term_incentive"]
-
-        # prevent zero division error
-        if v["to_messages"] == 0:
-            v["poi_comm_index"] = 0
-        else:
-            v["poi_comm_index"] = float(v["from_this_person_to_poi"] / v["to_messages"])
+# see jupyter notebook
 
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -62,10 +61,10 @@ from sklearn.model_selection import train_test_split
 
 # scale features
 #features = StandardScaler().fit_transform(features)
-features = MinMaxScaler().fit_transform(features)
+features = StandardScaler().fit_transform(features)
 
 # perform train-test split
-features_train, features_test, labels_train, labels_test = train_test_split(features, labels,test_size=0.3, random_state=42)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
 # base clf
 from sklearn.linear_model import LinearRegression
@@ -77,29 +76,21 @@ pipe1 = Pipeline([
 ])
 
 
-
 # clf with feature selection
 # http://scikit-learn.org/stable/modules/feature_selection.html#univariate-feature-selection
-from sklearn.svm import LinearSVC
 
-
-pipe2a = Pipeline([
-    ("feautre_selection", SelectFromModel(LinearSVC())),
-    ("clf", GaussianNB())
-])
-
-# clf with feature selection and tree
 from sklearn.ensemble import RandomForestClassifier
 
 pipe2a = Pipeline([
-    ("feautre_selection", SelectKBest(k=4)),
+    #("feautre_selection", SelectKBest(k=4)),
     ("clf", RandomForestClassifier())
 ])
 
 #
 from sklearn.linear_model import LogisticRegressionCV
+
 cv = StratifiedShuffleSplit(n_splits=10)
-pipe2c = Pipeline([
+pipe2b = Pipeline([
     ("log_reg", LogisticRegressionCV(cv=cv, solver="liblinear")) # provided as base clf
 ])
 
@@ -111,31 +102,41 @@ pipe2c = Pipeline([
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 from sklearn.model_selection import GridSearchCV
-#clf = RandomForestClassifier()
-#params = ""
 
-#gs = GridSearchCV(clf, params=params, )
+clf = RandomForestClassifier()
+params = {
+    "max_depth": range(1,20+1,1),
+    "min_samples_leaf": range(1,20+1,1),
+  }
+
+gs = GridSearchCV(clf, param_grid=params, cv=cv, n_jobs=3)
+#gs.fit(features, labels)
+
+#print(gs.best_params_)
 
 
 # Example starting point. Try investigating other evaluation techniques!
 # fit clf
-clf = pipe1
-#clf.fit(features_train, labels_train)
-clf.fit(features, labels)
+
+#clf = pipe2c
+clf = RandomForestClassifier(max_depth=4, min_samples_leaf=2) # max_depth = 4, min_samples_leaf = 2
+
+clf.fit(features_train, labels_train)
+#clf.fit(features, labels)
 
 # predict results
-#pred = clf.predict(features_test)
-pred = clf.predict(features)
+pred = clf.predict(features_test)
+#pred = clf.predict(features)
 
 # print precision and recall score
 from sklearn.metrics import precision_score, recall_score, classification_report
 #print "precision: %f" % precision_score(pred, labels_test) # base score: 0.4
 #print "recall: %f" % recall_score(pred, labels_test) # base score: 0.4
-#print classification_report(labels_test, pred)
-print classification_report(labels, pred)
+print classification_report(labels_test, pred)
+#print classification_report(labels, pred)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-#dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
